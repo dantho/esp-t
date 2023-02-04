b025fa0a19f09e21c4cd292a46ae97e00c6a6725
Dan Thornton <dan.thornton+Git@thornton.me>
2023-02-04 10:18:46 -0500

    part 2 done. Accelerometer working!

--- adv/i2cs/src/main.rs
+++ adv/i2cs/src/main.rs
@@ -1,18 +1,42 @@
 use anyhow;
 use embedded_hal::blocking::delay::DelayMs;
+#[allow(unused_imports)]
 use esp_idf_hal::{
     delay::FreeRtos,
     i2c::{config::MasterConfig, Master, MasterPins, I2C0},
     peripherals::Peripherals,
     prelude::*,
+    mutex::Mutex,
 };
+#[allow(unused_imports)]
 use esp_idf_sys::*;
-use shtcx::{self, PowerMode};
+#[allow(unused_imports)]
+use shtcx::{self, PowerMode}; // driver for the Sensirion SHTCx T & H sensor series
+// use imc42670p; 
+#[allow(unused_imports)]
+use icm42670::{prelude::*, Address, Icm42670}; // driver for the ICM-42670 6-axis IMU from InvenSense
 
-// goals of this exercise:
+#[allow(unused_imports)]
+use core::fmt::Write;
+
+// // From icm42670 crate's example code, not training:
+// use esp32c3_hal::{
+//     clock::ClockControl,
+//     gpio::IO,
+//     i2c::I2C,
+//     pac::Peripherals,
+//     prelude::*,
+//     timer::TimerGroup,
+//     Delay,
+//     Rtc,
+//     UsbSerialJtag,
+// };
+
+// goals of part 1:
 // instantiate i2c peripheral
-// implement one sensor, print sensor values
-// implement second sensor on same bus to solve an ownership problem
+// implement one T&H sensor, print sensor values
+// goals of part 2:
+// implement IMU sensor on same bus to solve an ownership problem
 
 fn main() -> anyhow::Result<()>  {
     let peripherals = Peripherals::take().unwrap();
@@ -21,31 +45,42 @@ fn main() -> anyhow::Result<()>  {
     let sda = peripherals.pins.gpio10;
     let scl = peripherals.pins.gpio8; 
 
-    let i2c = Master::<I2C0, _, _>::new(
+    let i2c_singleton = Master::<I2C0, _, _>::new(
         peripherals.i2c0,
         MasterPins { sda, scl },
         <MasterConfig as Default>::default().baudrate(400.kHz().into()),
     )?;
-   
-    // Create an instance of the SHTC3 sensor, find help in the documentation.
-    let mut sht = shtcx::shtc3(i2c);
 
-    // Read and print the sensor's device ID, find the methods in the documentation.
+    // Instantiate the bus manager, pass the i2c bus. 
+    let i2c_bus = shared_bus::BusManagerSimple::new(i2c_singleton);
+
+    // Create two proxies. Now, each sensor can have their own instance of a proxy i2c, which resolves the ownership problem. 
+    let i2c_bus_seat1 =i2c_bus.acquire_i2c(); // a proxy for the i2c bus
+    let i2c_bus_seat2 =i2c_bus.acquire_i2c(); // a 2nd proxy for the same i2c bus
+
+    // Create an instance of the SHTC3 sensor, find help in the documentation.
+    let mut sht = shtcx::shtc3(i2c_bus_seat1);
     let device_id = sht.device_identifier().unwrap();
     println!("SHTC3 T&H Device ID: {}", device_id);
+
+    // Create an instance of the IMU, find help via 'cargo doc --open'
+    let mut imu = Icm42670::new(i2c_bus_seat2, Address::Primary).unwrap();
+    let device_id = imu.device_id().unwrap();
+    println!("ICM42670 IMU Device ID: {}", device_id);
 
     loop {
         // This loop initiates measurements, reads values and prints humidity in % and Temperature in °C.
         sht.start_measurement(PowerMode::NormalMode).unwrap();
         FreeRtos.delay_ms(100u32);
-        let measurement = sht.get_measurement_result().unwrap(); 
-        
-        println!(
-            "TEMP: {} °C\n
-            HUM: {:?} %\n
-            \n",
-            measurement.temperature.as_degrees_celsius(), measurement.humidity.as_percent(),
-        );
+        let th_meas = sht.get_measurement_result().unwrap(); 
+
+        let acc_meas = imu.accel_norm().unwrap();
+
+        println!("TEMP: {:.2} °C", th_meas.temperature.as_degrees_celsius());
+        println!("HUM: {:.2} %", th_meas.humidity.as_percent());
+        println!("X: {:.3}, Y: {:.3}, Z: {:.3} g's", acc_meas.x, acc_meas.y, acc_meas.z);
+        println!(" ");
+
         FreeRtos.delay_ms(500u32);
     }
 }
